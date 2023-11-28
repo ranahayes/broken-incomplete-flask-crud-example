a9916a235f9f0b48e6ca5ae7cabe76163013f7aa
Luke Kiernan <120487731+LukeKiernan234@users.noreply.github.com>
2023-11-28 12:54:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 app.config['MYSQL_USER'] = 'A'
 app.config['MYSQL_PASSWORD'] = 'B'
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = 'db.mydomain.ie'
+app.config['MYSQL_HOST'] = '34.125.223.69'
 mysql.init_app(app)
 
 @app.route("/add") #Add Student
