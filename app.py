cd162ba83f7b788894d2da19a6670cab5144e22b
Luke Kiernan <120487731+LukeKiernan234@users.noreply.github.com>
2023-11-29 11:04:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'secret'
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = '34.125.223.69'
+app.config['MYSQL_HOST'] = '34.125.170.207'
 mysql.init_app(app)
 
 def execute_query(query):
